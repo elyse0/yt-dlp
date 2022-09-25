4edd8a764be4f9fbbd63879ec1c7bd81259ff7b4
Elyse <26639800+elyse0@users.noreply.github.com>
2022-09-25 18:10:59 -0500

    [downloader/hls] Don't finish frag download if it's a live

--- yt_dlp/downloader/fragment.py
+++ yt_dlp/downloader/fragment.py
@@ -527,5 +527,8 @@ class FragmentFD(FileDownloader):
         if finish_func is not None:
             ctx['dest_stream'].write(finish_func())
             ctx['dest_stream'].flush()
-        self._finish_frag_download(ctx, info_dict)
+
+        if not ctx.get('live'):
+            self._finish_frag_download(ctx, info_dict)
+
         return True
